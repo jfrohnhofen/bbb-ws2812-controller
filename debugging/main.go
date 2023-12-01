1b518ae474dae762d7f5aad1c44977d681a60b41
Johannes Frohnhofen <jf@daedalean.ai>
2023-12-01 10:09:47 +0100

    wip

--- debugging/main.go
+++ debugging/main.go
@@ -4,7 +4,11 @@ import (
 	"encoding/binary"
 	"fmt"
 	"log"
+	"net"
+	"os"
 	"os/exec"
+	"regexp"
+	"strconv"
 	"time"
 
 	"github.com/aamcrae/pru"
@@ -13,12 +17,56 @@ import (
 //go:generate ../build/pasm -V3 -g -Gmain -Cpru0Code pru0.p
 //go:generate ../build/pasm -V3 -g -Gmain -Cpru1Code pru1.p
 
+type Frame struct {
+	numChannels uint32
+	data        []byte
+}
+
+type PruBuffer struct {
+	addr uint32
+	size uint32
+	data []byte
+	next *PruBuffer
+}
+
+const (
+	recorderActive = false
+
+	port = 9000
+
+	frameDataOffset = 12
+	bytesPerLed     = 3
+	bitsPerLed      = 24
+
+	numChannelsPerDataPin = 6
+	numDataPins           = 8
+
+	pruEndAddrOffset  = 0
+	pruNextAddrOffset = 4
+	pruDataOffset     = 8
+
+	perBitTime = 1250 * time.Nanosecond
+	resetTime  = 1 * time.Millisecond
+)
+
 var (
+	prus       *pru.PRU
+	ch         = make(chan bool)
 	pruInputs  = []string{"P9_27", "P9_28", "P9_29", "P9_30", "P9_31"}
 	pruOutputs = []string{"P8_27", "P8_28", "P8_29", "P8_30", "P8_39", "P8_40", "P8_41", "P8_42", "P8_43", "P8_44", "P8_45", "P8_46"}
+	showRe     = regexp.MustCompile(`^show ([a-zA-Z0-9_\./-]*) (\d+)$`)
+	playRe     = regexp.MustCompile(`^play ([a-zA-Z0-9_\./-]*) (\d+) to (\d+) @ (\d+) fps$`)
+	stopRe     = regexp.MustCompile(`^stop$`)
 )
 
 func main() {
+	setupPrus()
+	go releaseToken()
+	play("/mnt/sdcard/Render_08-11-22_adafruit.led", 0, 600, 25)
+	//runServer()
+}
+
+func setupPrus() {
 	for _, pin := range pruOutputs {
 		if err := exec.Command("config-pin", pin, "pruout").Run(); err != nil {
 			log.Fatalf("Failed to configure pin '%s': %s\n", pin, err)
@@ -30,39 +78,158 @@ func main() {
 		}
 	}
 
-	prus, err := pru.Open(pru.NewConfig().EnableUnit(0).EnableUnit(1))
+	var err error
+	prus, err = pru.Open(pru.NewConfig().EnableUnit(0).EnableUnit(1))
 	if err != nil {
 		log.Fatalf("Failed to open PRU device: %s\n", err)
 	}
 
-	pru0 := prus.Unit(0)
-	if err := pru0.LoadAt(pru0Code, 0x00); err != nil {
+	if err := prus.Unit(0).LoadAt(pru0Code, 0x00); err != nil {
 		log.Fatalf("Failed to load PRU0 code: %s\n", err)
 	}
 
-	pru1 := prus.Unit(1)
-	if err := pru1.LoadAt(pru1Code, 0x00); err != nil {
+	if err := prus.Unit(1).LoadAt(pru1Code, 0x00); err != nil {
 		log.Fatalf("Failed to load PRU1 code: %s\n", err)
 	}
+}
 
-	binary.LittleEndian.PutUint32(pru0.Ram, 0)
-	pru0.RunAt(0)
-	pru1.RunAt(0)
+func runServer() {
+	conn, err := net.ListenPacket("udp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
+	log.Printf("Listening on port %d\n", port)
+
+	for {
+		buf := make([]byte, 1024)
+		n, addr, err := conn.ReadFrom(buf)
+		if err != nil {
+			log.Printf("Failed to read from connection: %s\n", err)
+			continue
+		}
+		go handle(conn, addr, string(buf[:n-1]))
+	}
+}
+
+func handle(conn net.PacketConn, addr net.Addr, cmd string) {
+	log.Printf("Command from %s: '%s'\n", addr, cmd)
+
+	if match := showRe.FindStringSubmatch(cmd); match != nil {
+		frame, _ := strconv.ParseInt(match[2], 10, 32)
+		show(match[1], uint32(frame))
+	} else if match := playRe.FindStringSubmatch(cmd); match != nil {
+		from, _ := strconv.ParseInt(match[2], 10, 32)
+		to, _ := strconv.ParseInt(match[3], 10, 32)
+		fps, _ := strconv.ParseInt(match[4], 10, 32)
+		play(match[1], uint32(from), uint32(to), uint32(fps))
+	} else if stopRe.MatchString(cmd) {
+		stop()
+	} else {
+		log.Printf("Invalid command: '%s'\n", cmd)
+	}
+}
+
+func show(input string, frame uint32) {
+	claimToken()
+	for hasToken() {
+		if err := outputFrame(input, frame); err != nil {
+			log.Printf("Failed to show frame: %s", err)
+		} else {
+			log.Printf("Done showing frame %d", frame)
+			releaseToken()
+			return
+		}
+	}
+}
+
+func play(input string, from, to, fps uint32) {
+	claimToken()
 	start := time.Now()
-	for (pru0.IsRunning() || pru1.IsRunning()) && time.Since(start) < time.Second {
+	for hasToken() {
+		frame := from + uint32(time.Since(start).Seconds()*float64(fps))
+		if frame > to {
+			frame = to
+		}
+
+		if err := outputFrame(input, frame); err != nil {
+			log.Printf("Failed to show frame: %s", err)
+		} else if frame == to {
+			log.Printf("Done playing frames %d to %d", from, to)
+			releaseToken()
+			return
+		}
+
+		nextFrameStart := start.Add(time.Duration(float64(frame-from+1) * float64(time.Second) / float64(fps)))
+		time.Sleep(time.Until(nextFrameStart))
+	}
+}
+
+func stop() {
+	claimToken()
+	releaseToken()
+}
+
+func outputFrame(input string, frameIdx uint32) error {
+	frame, err := loadFrame(input, frameIdx)
+	if err != nil {
+		return fmt.Errorf("failed to load frame '%s:%d': %s", input, frameIdx, err)
+	}
+
+	if frame.numChannels != numChannelsPerDataPin*numDataPins {
+		return fmt.Errorf("frame has invalid number of cahnnels")
+	}
+
+	sharedRam := PruBuffer{addr: 0x0001_0000, size: 12 * 1024, data: prus.SharedRam}
+	pru1Ram := PruBuffer{addr: 0x0000_0000, size: 8 * 1024, data: prus.Unit(1).Ram, next: &sharedRam}
+	sharedRam.next = &pru1Ram
+	buffers := []PruBuffer{pru1Ram, sharedRam}
+
+	numBits := len(frame.data) / numChannelsPerDataPin
+	pruTimeout := time.Duration(numBits)*perBitTime + resetTime
+
+	for _, buf := range buffers {
+		binary.LittleEndian.PutUint32(buf.data[pruEndAddrOffset:], 0x0000_0000)
+		binary.LittleEndian.PutUint32(buf.data[pruNextAddrOffset:], buf.next.addr)
+		if len(frame.data) > 0 {
+			fillPruBuffer(&frame, buf)
+		}
+	}
+
+	prus.Unit(0).RunAt(0)
+	prus.Unit(1).RunAt(0)
+	start := time.Now()
+
+	buf := &buffers[0]
+	for prus.Unit(1).IsRunning() && time.Since(start) < pruTimeout {
+		if len(frame.data) > 0 && binary.LittleEndian.Uint32(buf.data[pruEndAddrOffset:]) == 0x0000_0000 {
+			fillPruBuffer(&frame, *buf)
+			buf = buf.next
+		}
 		time.Sleep(10 * time.Microsecond)
 	}
 
-	if pru0.IsRunning() {
-		log.Println("PRU0 is still busy")
-		return
+	if len(frame.data) > 0 {
+		return fmt.Errorf("controller has not written all data")
 	}
-	count0 := binary.LittleEndian.Uint32(pru0.Ram[0:]) - 4
-	fmt.Println("PRU0 cycles:", count0)
 
-	samples := pru0.Ram[4 : 4*4*28+4]
+	if prus.Unit(1).IsRunning() {
+		return fmt.Errorf("PRU timed out")
+	}
+	for _, buf := range buffers {
+		if binary.LittleEndian.Uint32(buf.data[pruEndAddrOffset:]) != 0x0000_0000 {
+			return fmt.Errorf("PRU has not written all data")
+		}
+	}
 
+	if !recorderActive {
+		return nil
+	}
+	if prus.Unit(0).IsRunning() {
+		return fmt.Errorf("Recorder timed out")
+	}
+	samples := prus.Unit(0).Ram[4 : 4*4*28+4]
 	for burst := 0; burst < 4; burst++ {
 		for ch := 0; ch < 6; ch++ {
 			fmt.Printf("CH%d ", ch)
@@ -78,12 +245,65 @@ func main() {
 		fmt.Println()
 	}
 
-	if pru1.IsRunning() {
-		log.Println("PRU1 is still busy")
-	}
-	count1 := binary.LittleEndian.Uint32(pru1.Ram[0:])
-	sum1 := binary.LittleEndian.Uint32(pru1.Ram[4:]) - 4*count1
-	fmt.Println("PRU1 time: count", count1, "sum", sum1, "avg", float64(sum1)/float64(count1))
+	return nil
+}
 
-	fmt.Println("Done")
+func loadFrame(input string, frameIdx uint32) (Frame, error) {
+	frame := Frame{}
+
+	file, err := os.Open(input)
+	if err != nil {
+		return frame, err
+	}
+	defer file.Close()
+
+	var numFrames, numLeds uint32
+	if err := binary.Read(file, binary.LittleEndian, &frame.numChannels); err != nil {
+		return frame, err
+	}
+	if err := binary.Read(file, binary.LittleEndian, &numLeds); err != nil {
+		return frame, err
+	}
+	if err := binary.Read(file, binary.LittleEndian, &numFrames); err != nil {
+		return frame, err
+	}
+	if frameIdx >= numFrames {
+		return frame, fmt.Errorf("frame index out of range")
+	}
+
+	size := frame.numChannels * numLeds * bytesPerLed
+	offset := int64(frameDataOffset + frameIdx*size)
+	frame.data = make([]byte, size)
+	_, err = file.ReadAt(frame.data, offset)
+	return frame, err
+}
+
+func fillPruBuffer(frame *Frame, buf PruBuffer) {
+	numBits := (buf.size - pruDataOffset) / numChannelsPerDataPin
+	if bitsLeft := uint32(len(frame.data)) / numChannelsPerDataPin; bitsLeft <= numBits {
+		numBits = bitsLeft
+	}
+	numBits = numBits / bitsPerLed * bitsPerLed
+	size := numBits * numChannelsPerDataPin
+	endAddr := buf.addr + pruDataOffset + size - numChannelsPerDataPin
+	binary.LittleEndian.PutUint32(buf.data[pruEndAddrOffset:], endAddr)
+	copy(buf.data[pruDataOffset:], frame.data[:size])
+	frame.data = frame.data[size:]
+}
+
+func claimToken() {
+	<-ch
+}
+
+func hasToken() bool {
+	select {
+	case ch <- true:
+		return false
+	default:
+		return true
+	}
+}
+
+func releaseToken() {
+	ch <- true
 }
--- debugging/pru1.p
+++ debugging/pru1.p
@@ -1,37 +1,56 @@
 .origin 0
 .entrypoint start
 
+#define UINT32 4
+
+#define NUM_CHANNELS 6
+#define RESET_CYCLES 200000
+
+#define CTRL_BASE_ADDR 0x24000
+#define CTRL_CONTROL_REG 0x00
+#define CTRL_CYCLE_REG 0x0c
+#define CTRL_COUNTER_EN_BIT 3
+
+// frame data
+#define END_ADDR_OFFSET 0
+#define NEXT_ADDR_OFFSET 4
+#define DATA_OFFSET 8
+
+// pins
+#define STORE_CLOCK_PIN 8
+#define SHIFT_CLOCK_PIN 9
+#define TRIGGER_PIN 10
+
+// registers
+#define tmp r0
+#define buf_addr r1
+#define ctrl r2
+#define data0 r3
+#define data1 r4
+#define end_addr r6
+#define data_addr r9
+#define cnt0 r7
+#define cnt1 r8
+#define zeros r10
+#define ones r11
 #define gpio r30
-
-#define SHIFT    9
-#define STORE    8
-#define DATA0    0
-#define DATA1    1
-#define TRIGGER 10
-
-#define data0 r1
-#define data1 r2
-#define ones r3
-#define zeros r4
 
 .macro nop
 .mparam n
     loop exit, n-1
-    mov     r0, r0
+    mov     tmp, tmp
 exit:
 .endm
-
 
 .macro shift
 .mparam data
     mov     gpio.b0, data
     nop     2
-    set     gpio, SHIFT
+    set     gpio, SHIFT_CLOCK_PIN
     nop     2
-    clr     gpio, SHIFT
+    clr     gpio, SHIFT_CLOCK_PIN
     nop     2
 .endm
-
 
 .macro output
     shift   data0.b0
@@ -42,38 +61,79 @@ exit:
     shift   data1.b1
     shift   data1.b2
     shift   data1.b3
-    set     gpio, STORE
+    set     gpio, STORE_CLOCK_PIN
     nop     2
-    clr     gpio, STORE
+    clr     gpio, STORE_CLOCK_PIN
 .endm
 
 
 start:
-    mov     gpio, 0x00
-
-    mov     ones, 0xffffffff
+    // constants
     mov     zeros, 0x00000000
+    mov     ones, 0xffffffff
 
-    set     gpio, TRIGGER
-    nop     100
-    clr     gpio, TRIGGER
-    
+    // clear pins
+    //mov     gpio, zeros
+
+    // enable cycle counter
+    mov     ctrl, CTRL_BASE_ADDR
+    lbbo    tmp, ctrl, CTRL_CONTROL_REG, UINT32
+    set     tmp, CTRL_COUNTER_EN_BIT
+    sbbo    tmp, ctrl, CTRL_CONTROL_REG, UINT32
+
+    // set address and params of first buffer
+    mov     buf_addr, 0x00000000
+    lbbo    end_addr, buf_addr, END_ADDR_OFFSET, UINT32
+    add     data_addr, buf_addr, DATA_OFFSET
+
+    set     gpio, TRIGGER_PIN
+
 loop_bits:
+    // output ones
     mov     data0, ones
     mov     data1, ones
     output
-    nop     20
+    nop     2
 
-    mov     data0, 0x000000
-    mov     data1, 0x000000
+    // output data
+    lbbo    data0, data_addr, 0, NUM_CHANNELS
     output
-
+    
+    // output zeros
     mov     data0, zeros
     mov     data1, zeros
     output
-
-    nop  11
-
-    jmp loop_bits
     
+    // check exit condition
+    qbeq    next_buffer, data_addr, end_addr
+    add     data_addr, data_addr, NUM_CHANNELS
+    
+    nop 9
+    jmp     loop_bits
+
+next_buffer:
+    // clear end_addr
+    sbbo    zeros, buf_addr, END_ADDR_OFFSET, UINT32
+
+    // load address of next buffer
+    lbbo    buf_addr, buf_addr, NEXT_ADDR_OFFSET, UINT32
+
+    // set params of next buffer
+    lbbo    end_addr, buf_addr, END_ADDR_OFFSET, UINT32
+    add     data_addr, buf_addr, DATA_OFFSET
+
+    // check exit condition
+    qbeq    exit, end_addr, zeros
+
+    jmp     loop_bits
+
+exit:
+    nop 4
+
+    mov     tmp, RESET_CYCLES
+reset:
+    sub     tmp, tmp, 2
+    qbne    reset, tmp, 0
+
+    clr     gpio, TRIGGER_PIN
     halt
